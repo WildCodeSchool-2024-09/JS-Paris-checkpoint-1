2db3e755046cec8a19fcb5fd4edb6c7e04be6d69
MARINA MASSOT <marina.massot1@gmail.com>
2024-10-21 12:29:40 +0200

    finish JS step 3 and 4 in part 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -10,11 +10,18 @@ imgAvatar.addEventListener("click", () => {
 //   changer le nom et la couleur
 const firstName = document.querySelector("#firstname");
 const button = document.querySelector("#replaceName");
-// const inputField = document.querySelector("#nameInput");
+const paragraphs = document.querySelectorAll(".description.pink-bg")
+const textColor = document.querySelectorAll(".pink-text")
 
 button.addEventListener("click", () => {
 	const userInput = prompt("Please enter some text:");
 	console.log(userInput);
 	firstName.textContent = userInput;
 	firstName.style.color = "white";
-});
+    paragraphs.forEach((element) => {
+		element.style.backgroundColor = "#750ff7";
+	});
+    textColors.forEach((element) => {
+		element.style.color = "#750ff7";
+	});
+})

5a97db60680833a99361e69e7850a3b742cabea6
Mickaël Lambert <mickael.lambert@wildcodeschool.com>
2024-10-21 16:44:36 +0200

    J'AI FINI

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -9,10 +9,16 @@ const editFirstnameButton = document.querySelector('#edit-firstname')
 editFirstnameButton.addEventListener('click', function() {
     let color = prompt('Couleur : ')
 
-    const backgrounds = document.querySelectorAll('.description.pink-bg')
-
+    const backgrounds = document.querySelectorAll('.pink-bg')
+    
     for (let background of backgrounds) {
         background.style.backgroundColor = color;
+    }
+
+    const texts = document.querySelectorAll('.pink-text')
+
+    for (let text of texts) {
+        text.style.color = color;
     }
 
     let firstname = prompt('Ton prénom :');

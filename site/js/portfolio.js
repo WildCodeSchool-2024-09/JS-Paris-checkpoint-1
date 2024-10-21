13364ac9b5e3da5817b5c5c26e41cf4efa27f31e
Mickaël Lambert <mickael.lambert@wildcodeschool.com>
2024-10-21 16:34:02 +0200

    Step 3 JS : Modify Text & Color

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -4,4 +4,23 @@ imageAvatar.addEventListener('click', function() {
     imageAvatar.src = imageAvatar.src.endsWith('png') ? 'image/avatar.svg' : 'image/avatar-bis.png';
 });
 
-// Etape 3 : Modifier la source de l'image
+const editFirstnameButton = document.querySelector('#edit-firstname')
+
+editFirstnameButton.addEventListener('click', function() {
+    let color = prompt('Couleur : ')
+
+    const backgrounds = document.querySelectorAll('.description.pink-bg')
+
+    for (let background of backgrounds) {
+        background.style.backgroundColor = color;
+    }
+
+    let firstname = prompt('Ton prénom :');
+    let actualFirstname = document.querySelector('#firstname')
+
+    actualFirstname.textContent = firstname;
+    actualFirstname.style.color = 'white';
+});
+
+
+// Etape 7 : Remplacer + couleur

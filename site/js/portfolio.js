bf11ef65d80a3d98e1ba93369f204c7bada4044b
MARINA MASSOT <marina.massot1@gmail.com>
2024-10-21 11:21:54 +0200

    finish JS step 1 in part 2

---
+++ site/js/portfolio.js
@@ -0,0 +1,6 @@
+const imgAvatar = document.querySelector('#avatar');
+
+imgAvatar.addEventListener('click', function () {
+    imgAvatar.src = "image/avatar.svg";
+    imgAvatar.alt = "Avatar 2";
+  });

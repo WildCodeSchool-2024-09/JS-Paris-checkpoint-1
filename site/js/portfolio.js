6602386270ffc4cea45dafdc004a46c2b7859b4e
MARINA MASSOT <marina.massot1@gmail.com>
2024-10-21 12:01:24 +0200

    finish JS step 2  in part 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,6 +1,20 @@
-const imgAvatar = document.querySelector('#avatar');
+// changer l'image de l'avatar
 
-imgAvatar.addEventListener('click', function () {
-    imgAvatar.src = "image/avatar.svg";
-    imgAvatar.alt = "Avatar 2";
-  });
+const imgAvatar = document.querySelector("#avatar");
+
+imgAvatar.addEventListener("click", () => {
+	imgAvatar.src = "image/avatar.svg";
+	imgAvatar.alt = "Avatar 2";
+});
+
+//   changer le nom et la couleur
+const firstName = document.querySelector("#firstname");
+const button = document.querySelector("#replaceName");
+// const inputField = document.querySelector("#nameInput");
+
+button.addEventListener("click", () => {
+	const userInput = prompt("Please enter some text:");
+	console.log(userInput);
+	firstName.textContent = userInput;
+	firstName.style.color = "white";
+});

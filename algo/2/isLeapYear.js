77478e05ae46970d62fe4f5d5a3e408d45d1ea80
TIMOTHE ESPOSITO <timotheesposito@gmail.com>
2024-10-21 09:59:47 +0200

    finish-algo-2

--- algo/2/isLeapYear.js
+++ algo/2/isLeapYear.js
@@ -8,7 +8,18 @@ Rappel : Pour vérifier si un nombre est divisible par un autre, tu peux utilise
 */
 
 function isLeapYear(year) {
-  // Your code here !
+  if (year % 4 === 0) {
+
+    if (year % 100 !== 0) {
+
+      return true;
+    } else {
+      
+      return year % 400 === 0; 
+    }
+  }
+  return false;
 }
+
 
 module.exports = isLeapYear;
